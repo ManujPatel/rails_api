376a9d12cacc57b2d29d15e6c0c3111a5c794de1
Manuj Patel <manuj.p@simformsolutions.com>
2022-04-04 23:58:27 +0530

    created rails api application,created article model and created jason response for it

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,38 @@
+module Api
+    module V1
+        class ArticlesController < ApplicationController
+            def index 
+                @articles = Article.order('created_at DESC')
+                render json: {status: 'success', message:'loaded articles', data:@articles},status: :ok
+            end
+
+            def show
+                @article = Article.find(params[:id])
+                render json: {status: 'success', message:'loaded article', data:@article},status: :ok
+            end       
+            
+            def create
+                @article = Article.new(article_params)
+                if @article.save
+                    render json: {status: 'success', message:'saved article', data:@article},status: :ok
+                else
+                    render json: {status: 'Error', message:'article not saved', data:@article.errors},status: :unprocessable_entity
+                end
+            end
+
+
+            def destroy
+                @article = Article.find(params[:id])
+                @article.destroy
+                render json: {status: 'success', message:'deleted article', data:@article},status: :ok
+            end
+
+            private
+
+                def article_params
+                    params.permit(:title, :body, :release_date)
+                end
+
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace 'api' do
+    namespace 'v1' do
+      resources :articles
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
